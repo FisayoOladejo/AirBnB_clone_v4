b97589705ebe761d447f7e159a6cad3fb7e3c6de
Fisayo Oladejo <108827240+FisayoOladejo@users.noreply.github.com>
2023-05-08 17:57:46 +0100

    Update 0-hbnb.py

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,13 +1,32 @@
-cript using bash to setup webservers for deployment of simple HTML
-# content
+#!/usr/bin/python3
+"""Flask app to generate complete html page containing location/amenity
+dropdown menus and rental listings"""
+from flask import Flask, render_template
+from models import storage
+import uuid
+app = Flask('web_dynamic')
+app.url_map.strict_slashes = False
 
-apt-get update && \
-apt-get install -y nginx && \
-mkdir -p -m=755 /data/web_static/{releases/test,shared} || exit 0
-echo 'Testing 123' > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data/
-insert='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
-sed -i "37i $insert" /etc/nginx/sites-available/default
-service nginx restart
-exit 0
+
+@app.route('/0-hbnb')
+def display_hbnb():
+    """Generate page with popdown menu of states/cities"""
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    cache_id = uuid.uuid4()
+    return render_template('0-hbnb.html',
+                           states=states,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
